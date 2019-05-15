49d57552d48dd038fee851d9aa4545fcf0a57101
Majdi Saibi <saibimajdi@outlook.com>
2019-05-15 14:23:53 -0600

    fixed the build.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using TestDevOps.Models;
-asdasdasdasdasdasd
+
 namespace TestDevOps.Controllers
 {
     public class HomeController : Controller
